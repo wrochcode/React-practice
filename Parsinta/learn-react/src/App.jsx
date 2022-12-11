128a50522a60742ce60339b36132c5b44bdc8851
Wahyu Rochman <wrochcode@gmail.com>
2022-12-11 20:12:48 +0700

    make component button

--- Parsinta/learn-react/src/App.jsx
+++ Parsinta/learn-react/src/App.jsx
@@ -1,7 +1,67 @@
+import {
+  IconBrandFacebook,
+  IconBrandGithub,
+  IconBrandGoogle,
+  IconBrandTwitter,
+} from '@tabler/icons'
+import clsx from 'clsx'
+
 export default function App() {
+  const type = 'submit'
+  const onClick = () => console.info(`Hallo`)
   return (
-    <div className="bg-blue-600 grid place-content-center min-h-screen">
-      <h1 className='text-xl'>Learn React</h1>
+    <div className='bg-slate-900 grid place-content-center min-h-screen'>
+      <div className='flex gap-x-2'>
+        <Button className='bg-green-700' text='Sign Up' />
+
+        <Button
+          {...{
+            className: 'bg-blue-600',
+            onClick: () => console.info(`Hallo`),
+            type: 'submit',
+          }}
+        >
+          <IconBrandTwitter />
+          Login
+        </Button>
+        <Button
+          className={'bg-red-600'}
+          onClick={() => console.info(`Hallo`)}
+          type='reset'
+        >
+          <IconBrandGoogle />
+          Login
+        </Button>
+        <Button className={'bg-blue-900 '} type='submit'>
+          <IconBrandFacebook />
+          Login
+        </Button>
+        <Button type='submit'>
+          <IconBrandGithub />
+          Login
+        </Button>
+      </div>
     </div>
+  )
+}
+
+function Button(props) {
+  const {
+    text,
+    children,
+    className = 'bg-black',
+    type = 'submit',
+  } = props
+  return (
+    <button
+      {...props}
+      type={type}
+      className={clsx(
+        className,
+        ` [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center bg-blue-300 text-white px-4 py-2 gap-x-2 rounded`
+      )}
+    >
+      {text || children}
+    </button>
   )
 }
--- Parsinta/learn-react/tailwind.config.cjs
+++ Parsinta/learn-react/tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+module.exports =
+  {
+    content:
+      [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+      ],
+    theme:
+      {
+        extend:
+          {},
+      },
+    plugins:
+      [],
+  }
