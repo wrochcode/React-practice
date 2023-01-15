d939a16b555d4d27e533a138e22b2d77c47244b3
Wahyu Rochman <wrochcode@gmail.com>
2023-01-15 22:28:08 +0700

    make button component

--- Parsinta/learn-react/src/App.jsx
+++ Parsinta/learn-react/src/App.jsx
@@ -1,7 +1,59 @@
+import {
+  IconBrandFacebook,
+  IconBrandGithub,
+  IconBrandTwitter,
+} from '@tabler/icons'
+import clsx from 'clsx'
+
 export default function App() {
+  const type = 'submit'
+  const onClick = () => console.log('twitter was clicked with another style')
   return (
-    <div className={'bg-blue-600 grid place-content-center min-h-screen'}>
-      <h1 className={'text-xl'}>Learn react</h1>
+    <div className={'bg-slate-900 grid place-content-center min-h-screen'}>
+      <div className={'flex gap-x-2'}>
+        <Button text='Register'></Button>
+        <Button {...{ onClick, type }} className='bg-slate-600'>
+          <>
+            <IconBrandTwitter />
+            Login
+          </>
+        </Button>
+        <Button
+          className={`bg-pink-600`}
+          onClick={() => console.log('facebook')}
+        >
+          <>
+            <IconBrandFacebook />
+            Login
+          </>
+        </Button>
+        <Button
+          className={`bg-black`}
+          onClick={() => console.log('black')}
+          type='submit'
+        >
+          <>
+            <IconBrandGithub />
+            Login
+          </>
+        </Button>
+      </div>
     </div>
+  )
+}
+
+function Button(props) {
+  const { className = `bg-blue-600`, children, text, type = `submit` } = props
+  return (
+    <button
+      {...props}
+      type={type}
+      className={clsx(
+        className,
+        '${className} [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2  text-white px-4 py-2 rounded'
+      )}
+    >
+      {text || children}
+    </button>
   )
 }
