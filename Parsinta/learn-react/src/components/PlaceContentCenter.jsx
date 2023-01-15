b8a4f87377af6e3fd1d523295ce31a536eb97a66
Wahyu Rochman <wrochcode@gmail.com>
2023-01-15 18:01:07 +0700

    Create learn react again

--- Parsinta/learn-react/src/components/PlaceContentCenter.jsx
+++
@@ -1,9 +0,0 @@
-export default function PlaceContentCenter({ children }) {
-  return (
-    <div className='bg-black'>
-      <div className={'bg-violet-900/20 min-h-screen text-violet-50 flex items-center justify-center antialiased tracking-tight'}>
-        <div className={' max-w-lg w-full '}>{children}</div>
-      </div>
-    </div>
-  )
-}
