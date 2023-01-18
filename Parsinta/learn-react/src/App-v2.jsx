dc11d431155ed87287651ebd2aedcfdeab5c207a
Wahyu Rochman <wrochcode@gmail.com>
2023-01-18 10:13:46 +0700

    Next level of component

--- Parsinta/learn-react/src/App-v2.jsx
+++ Parsinta/learn-react/src/App-v2.jsx
@@ -3,12 +3,13 @@ import Counter from './components/Counter'
 import PlaceContentCenter from './components/PlaceContentCenter'
 
 function App() {
-  
+  const [name, setName] = useState('Excel')
+  //count:
+  const [count, setCount] = useState(0)
 
   return (
     <PlaceContentCenter>
-      <Counter initialValue={0} />
-      {/* <Counter/> */}
+      <Counter count={count} setCount={setCount} name={name} setName={setName} />
     </PlaceContentCenter>
   )
 }
