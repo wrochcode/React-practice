50e3c1d8c8525c599ae4e4222fd307280e585fe9
Wahyu Rochman <wrochcode@gmail.com>
2023-01-19 04:21:39 +0700

    Update todo with checklist fiture and somthing else

--- Parsinta/learn-react/src/components/Todo.jsx
+++ Parsinta/learn-react/src/components/Todo.jsx
@@ -10,23 +10,77 @@ function Todo() {
   function handleAddTask(e) {
     e.preventDefault()
     // console.info('new task')
-    setTasks((prevTasks) => [...prevTasks, newTask])
+    // setTasks((prevTasks) => [...prevTasks, newTask])
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      {
+        id: Math.floor(Math.random() * Date.now()),
+        name: newTask,
+        completed: false,
+      },
+    ])
+    // reset input column
+    setNewTask('')
+  }
+
+  function handleCompleteTask(id) {
+    // console.info(id)
+    const updateTask = tasks.map((task) => {
+      if (id === task.id) {
+        return {
+          ...task,
+          completed: !task.completed,
+          // completed: true,
+        }
+      }
+      return task
+    })
+
+    setTasks(updateTask)
+  }
+
+  function handleRemoveTask(id){
+    const removeTask = tasks.filter(task =>task.id !==id)
+    // console.info(removeTask)
+    setTasks(removeTask)
+
   }
 
   return (
     // <div>
-      <Card>
-        <Card.Title>Todo</Card.Title>
-        <Card.Body>
-          <form>
-            <div className='flex items-center gap-x-2'>
-              <Input value={newTask} onChange={(e) => setNewTask(e.target.value)} />
-              <Button text={'Add Task'} onClick={handleAddTask} />
-            </div>
-          </form>
-        </Card.Body>
-        <Card.Footer>You Have {tasks.length} Task</Card.Footer>
-      </Card>
+    <Card>
+      <Card.Title>Todo</Card.Title>
+      <Card.Body>
+        <form>
+          <div className='flex items-center gap-x-2'>
+            <Input value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+            <Button text={'Add Task'} onClick={handleAddTask} />
+          </div>
+        </form>
+        {tasks.length > 0 ? (
+          <ol className={' mt-5 space-y-1'}>
+            {tasks.map((task) => (
+              <li key={task.id} className={'flex items-center justify-between'}>
+                <span>
+                  {task.name}
+                  {task.completed ? ' ✅' : ' ❌'}
+                  {/* {task.completed ? ' Complete' : ' Incomplete'} */}
+                </span>
+                <div className='flex items-center gap-x-2'>
+                  <button onClick={() => handleCompleteTask(task.id)} className='px-2 py-1 border text-xs'>
+                    Mark as {task.completed? 'Complete':'Incomplete'}
+                  </button>
+                  <button onClick={() => handleRemoveTask(task.id)} className='px-2 py-1 border text-xs'>
+                    Remove
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ol>
+        ) : null}
+      </Card.Body>
+      <Card.Footer>You Have {tasks.length} Task</Card.Footer>
+    </Card>
     // </div>
   )
 }
